4179f93f89ac64714f0ff98333e5e2020c5cdf0f
junglans <cderegistro@gmail.com>
2019-06-26 13:24:23 +0200

    Se añade un atributo de tipo Subscription para ejecutar un 'unsubscribe'

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import { WebsocketService } from '../../services/websocket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-
+/**
+ * Componente que se muestra en el pie y que informa del estado de la conexión con el servidor.
+ */
 export class FooterComponent implements OnInit, OnDestroy {
   
   private _connected: Boolean = false;
+  private _subscription: Subscription;
 
   constructor(private wsService: WebsocketService) {}
 
   ngOnInit(): void {
      this._connected = this.wsService.connected;
-     this.wsService.subject.subscribe((connected : Boolean) =>{ 
-       console.log(`Recibido evento de conexión/deconexión del servidor ${connected}`)
+     this._subscription = this.wsService.subject.subscribe((connected : Boolean) =>{ 
+       console.log(`Recibido evento de conexión/deconexión del servidor. Conectado :${connected}`)
        this._connected = connected
     });
   }
+
   ngOnDestroy(): void {
-     this.wsService.subject.unsubscribe();
+     this._subscription.unsubscribe();
   }
  
   public get connected(): Boolean {
