159d30341ffd86beb51b8d9a4e53c5300e50a436
junglans <cderegistro@gmail.com>
2019-06-28 11:28:28 +0200

    Se elimina el chat service y se acaba el componente de chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ChatService } from 'src/app/services/chat.service';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, QueryList } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TopicService } from 'src/app/services/topic/topic.service';
 
@@ -11,32 +10,64 @@ import { TopicService } from 'src/app/services/topic/topic.service';
 export class ChatComponent implements OnInit, OnDestroy {
  
   
+  private chatElement: HTMLElement;
+
   private _connected: Boolean = false;
-  private _subscription: Subscription;
+  private _subscriptionConnection: Subscription;
+  private _subscriptionMsgIn: Subscription;
 
   public text: string = '';
+  // List of received messages
+  public messages: Array<any> = new Array<any>();
 
-  constructor(private topicService: TopicService, private chatService: ChatService) {}
+  constructor(private topicService: TopicService) {}
 
   ngOnInit() { 
-    this._subscription = this.topicService.subscribe('SERVER_STATUS', (connected : Boolean) =>{ 
+
+    this._subscriptionConnection = this.topicService.subscribe('SERVER_STATUS', (connected : Boolean) =>{ 
       console.log(`ChatComponent> Recibido evento de conexión/deconexión del servidor. Conectado :${connected}`)
       this._connected = connected
+    });
+
+    this._subscriptionMsgIn = this.topicService.subscribe('MESSAGE_IN', (message : any) =>{ 
+      console.log('ChatComponent> Recibido mensaje entrante ' + message );
+      this.messages.push(message);
+      this.scrollToBottom(); 
     });
   }
 
   ngOnDestroy(): void {
 
-    if (this._subscription) {
-      this.topicService.unsubscribe(this._subscription);
+    if (this._subscriptionConnection) {
+      this.topicService.unsubscribe(this._subscriptionConnection);
+    }
+    if (this._subscriptionMsgIn) {
+      this.topicService.unsubscribe(this._subscriptionMsgIn);
     }
     
   }
+ 
 
   public sendMessage(): void {
     
-    this.chatService.sendMessage(this.text);
-    this.text = '';
+    if (this.text.trim().length != 0) {
+      
+      console.log('ChatComponent.sendMessage> Sending message:' + JSON.stringify(this.text));
+      this.topicService.publish('MESSAGE_OUT', this.text);
+      this.text = '';
 
+    }
+  }
+
+  ngAfterViewInit() {
+    this.chatElement  = document.getElementById('chat-messages');
+  }
+
+  private scrollToBottom(): any {
+    console.log('A-' + this.chatElement.scrollTop + ' ' + this.chatElement.scrollHeight);  
+    setTimeout(()=>{
+      this.chatElement.scrollTop =  this.chatElement.scrollHeight; 
+    },50);      
+     
   }
 }
